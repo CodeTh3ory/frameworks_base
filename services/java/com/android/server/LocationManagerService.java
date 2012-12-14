ea485f0f510ccbfd50493dcbf115076f481ba8eb
Dante Russo <drusso@codeaurora.org>
2012-12-13 16:37:03 -0800

    gps: fixed singleshot session that doesn't stop
    When a single shot location is requested and a
    location is returned, the location request is
    not stopped and so it continues even when the
    requestor has stopped listening. This can cause
    gps tracking to continue in the backgroud and eat
    up battery.
    
    This is fixed by stoping the provider if there
    are no more update record associated with it
    when a location is returned.
    
    Change-Id: Ie437732ff06231a46724843af30e21ca3a1694ab
    
    Conflicts:
    
    	services/java/com/android/server/LocationManagerService.java

--- services/java/com/android/server/LocationManagerService.java
+++ services/java/com/android/server/LocationManagerService.java
@@ -1062,9 +1062,9 @@ public class LocationManagerService extends ILocationManager.Stub implements Run
                     removeUpdatesLocked(mReceiver);
                 }
 
-	                // and also stop the provider if it has no more update records
+                // and also stop the provider if it has no more update records
                 if (globalRecords.isEmpty()) {
-	            applyRequirementsLocked(this.mProvider);
+                    applyRequirementsLocked(this.mProvider);
                 }
             }
         }
